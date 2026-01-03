d62bdef069fd582588b9f92da0e64f76667315fe
bhanu0240 <bhanureddy0240@gmail.com>
2026-01-03 23:08:49 +0530

    configuring application to be PWA

--- vite.config.js
+++ vite.config.js
@@ -10,34 +10,63 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: [
         "favicon.ico",
+        "favicon-96x96.png",
+        "favicon.svg",
         "apple-touch-icon.png",
         "web-app-manifest-192x192.png",
         "web-app-manifest-512x512.png",
       ],
       manifest: {
-        name: "My React Vite PWA",
-        short_name: "VitePWA",
-        description: "A Progressive Web App built with Vite + React",
+        name: "Destination Bir",
+        short_name: "Destination Bir",
+        description: "Hotel booking application for Destination Bir",
         theme_color: "#121212",
         background_color: "#ffffff",
         display: "standalone",
         start_url: "/",
         icons: [
           {
-            src: "web-app-manifest-192x192.png",
+            src: "/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any",
           },
           {
-            src: "web-app-manifest-512x512.png",
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
           },
           {
-            src: "web-app-manifest-512x512.png",
+            src: "/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico,json, woff,woff2}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/your-api-domain\.com\/.*$/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "api-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 5 * 60, // 5 minutes
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
           },
         ],
       },
