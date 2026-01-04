c71436b5b2119cf2d31f9a0f32b353c45938dda5
bhanu0240 <bhanureddy0240@gmail.com>
2026-01-04 12:25:53 +0530

    configured fronte end to point to prod backend

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiUrl: string = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: apiUrl,
   headers: { "Content-Type": "application/json" },
 });
 
